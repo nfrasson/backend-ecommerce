c4c712647f3ab0f6bf29f2b9e484c405eca1398f
Nicolas Frasson <nicolas.boaroto@timeprimo.com>
2023-12-20 15:39:06 -0300

    imp: remove old infrastructure

--- jest.config.js
+++
@@ -1,5 +0,0 @@
-export default {
-  testEnvironment: "node",
-  testRegex: "(/__tests__/(unit|integration)/.*|(\\.|/)(test|spec))\\.(js|mjs)$",
-  moduleFileExtensions: ["js", "mjs"],
-};
--- tmp/commons/utils/index.mjs
+++
@@ -1,6 +0,0 @@
-import Logger from "./logger.mjs";
-import { mergeBody } from "./mergeBody.mjs";
-import { returnHandler } from "./returnHandler.mjs";
-import { lambdaProcessor } from "./lambdaProcess.mjs";
-
-export { Logger, mergeBody, returnHandler, lambdaProcessor };
